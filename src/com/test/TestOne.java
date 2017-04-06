36034040d3fb14142ea97fa1a0cadb3bfb5bb2f9
zjfPro <1948787907@qq.com>
2017-04-06 21:12:16 +0800

    33

--- src/com/test/TestOne.java
+++ src/com/test/TestOne.java
@@ -3,12 +3,18 @@ package com.test;
 public class TestOne {
 
 	public static void main(String[] args) {
-		System.out.println("1122334445557777");
+		System.out.println("1122334445557777pppp");
+		System.out.println("2222222");
 		System.out.println("1122334445557777888899");
 	}
+	
 	public void dd(){
 		System.out.println("11");
 		System.out.println("2");
 	}
 
+	public void xx(){
+		System.out.println("1");
+		System.out.println("2");
+	}
 }
