aa1b9c9722e6897ae3cff30467b51f31e4452233
zjfPro <1948787907@qq.com>
2017-04-06 21:11:00 +0800

    Update TestOne.java
    gg

--- src/com/test/TestOne.java
+++ src/com/test/TestOne.java
@@ -3,12 +3,15 @@ package com.test;
 public class TestOne {
 
 	public static void main(String[] args) {
-		System.out.println("1122334445557777");
+		System.out.println("666");
 		System.out.println("1122334445557777888899");
 	}
 	public void dd(){
-		System.out.println("11");
+		System.out.println("16");
 		System.out.println("2");
 	}
-
+	public void oo(){
+		System.out.println("1");
+		System.out.println("2");
+	}
 }
